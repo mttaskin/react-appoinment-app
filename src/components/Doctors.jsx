ddd8e1c263e973f4bcac739615bbb682a7ea1329
mttaskin <0mttaskin1@gmail.com>
2023-02-27 21:35:43 +0300

    add modal

--- src/components/Doctors.jsx
+++ src/components/Doctors.jsx
@@ -1,15 +1,25 @@
 import Container from "react-bootstrap/Container"
 import Row from "react-bootstrap/Row"
 import Col from "react-bootstrap/Col"
+import { AddModal } from "./AddModal"
+import { useState } from "react"
 
 const Doctors = () => {
+  const [show, setShow] = useState(false)
+
+  // const handleClose = () => setShow(false)
+  // const handleShow = () => setShow(true)
+
+  const handleClick=()=>{setShow(true)}
+
   return (
     <Container className="p-2">
       <h3 className="display-6 mb-3" style={{color:"rgb(166,18,189)"}}>Our Doctors</h3>
       <Row className="justify-content-center">
         {Doctors.map((dr)=>(
           <Col key={dr.id} xs={6} sm={4} md={3} >
-            <img src={dr.img} alt={dr.name} className="img-thumbnail doctor-img" />
+            <img src={dr.img} alt={dr.name} className="img-thumbnail doctor-img" 
+            onClickP={handleClick} />
             <h5>{dr.name}</h5>
             <h6>{dr.dep}</h6>
         </Col>
@@ -17,6 +27,7 @@ const Doctors = () => {
         
 
       </Row>
+      <AddModal show={show} handleClose={()=> setShow(false)}/>
     </Container>
   )
 }
